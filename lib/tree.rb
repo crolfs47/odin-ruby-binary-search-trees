ac5336636d3bc59d227b4d9ea4021c8646a52dcf
Cathy Rolfs <crolfs47@gmail.com>
2022-12-14 16:07:12 -0600

    Add method to delete a node

--- lib/tree.rb
+++ lib/tree.rb
@@ -6,6 +6,12 @@ class Tree
   def initialize(array)
     @root = build_tree(array)
     pretty_print(@root)
+    # insert(600)
+    # insert(500)
+    # pretty_print(@root)
+    # p min_value_node(@root.right_child.right_child)
+    delete(67)
+    pretty_print(@root)
   end
 
   def build_tree(array)
@@ -28,8 +34,60 @@ class Tree
     pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
   end
 
+  def insert(value, node = root)
+    if root.nil?
+      Node.new(value)
+    elsif value == node.data
+      return nil
+    else
+      if value < node.data
+        node.left_child.nil? ? node.left_child = Node.new(value) : insert(value, node.left_child) 
+      else
+        node.right_child.nil? ? node.right_child = Node.new(value) : insert(value, node.right_child)
+      end
+    end
+  end
+
+  # return the node with the minimum value beneath node provided in method
+  def min_value_node(node)
+    current = node
+    current = current.left_child until current.left_child.nil?
+    current
+  end
+
+  # deletes the value provided and returns the new root
+  def delete(value, node = root)
+    return node if node.nil?
+
+    if value < node.data
+      node.left_child = delete(value, node.left_child)
+    elsif value > node.data
+      node.right_child = delete(value, node.right_child)
+    else
+      return node.right_child if node.left_child.nil?
+      return node.left_child if node.right_child.nil?
+
+      min_node = min_value_node(node.right_child)
+      node.data = min_node.data
+      node.right_child = delete(min_node.data, node.right_child)
+    end
+    node
+  end
+
 end
 
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 array2 = [5, 7, 1, 3, 2, 6, 4]
 Tree.new(array)
+
+# 3 cases
+# delete a leaf in the tree (end one)
+    # structure won't change, node that previously pointed to that tree won't anymore
+# delete a node with one child
+    # replace it with its child, point node's parent to its child
+# delete a node (Node A) with two children
+    # find thing in tree that's next biggest (Node X), the smallest thing in it's right subtree
+        # look in right subtree, node on very far left
+        # recursively remove Node X
+    # replace Node A with Node X
+
